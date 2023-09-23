d353f126e8cabcbd97967fe882e21a4cf401b614
Kauan <kauansantosdesouza45@gmail.com>
2023-09-23 14:37:54 -0300

    Inserido o pytest.ini para futuros testes e alterado a função menu na aba view.

--- core/views.py
+++ core/views.py
@@ -1,61 +1,57 @@
 from django.shortcuts import render
 from django.core.cache import cache
-from  database import banco
+from database import banco
 from aut.autenticacao import OtherSystemAuthBackend
 from django.contrib.sessions import base_session
-
 
 
 # Create your views here.
 cache.clear()
 # Identificar se o usuário existe
 
-def home(request ):
-    if request.method == 'POST':
-        registration = request.POST.get('registration')
-        password = request.POST.get('password')
 
-        user = OtherSystemAuthBackend.authenticate('', 
-                                                        request, 
-                                                        registration=registration.strip(), 
-                                                        password=password, 
-                                                        backend='autenticacao.criar_usuario.OtherSystemAuthBackend')
+def home(request):
+    if request.method == "POST":
+        registration = request.POST.get("registration")
+        password = request.POST.get("password")
+
+        # user = OtherSystemAuthBackend.authenticate(request,
+        #                                             registration=registration.strip(),
+        #                                             password=password,
+        #                                             backend='autenticacao.criar_usuario.OtherSystemAuthBackend')
         # result = banco.sql_query(f"""SELECT (*) FROM TB_FUNCIONARIOS WHERE matricula =  '{registration}' and senha = '{password}' """)
-        print(user)
-        if user is not None:
-        # Authenticates the user in the admin session
-            # login(request, user)
-            return render(request, 'menu.html')
-        else:
-            return render(request, 'pages/home.html', {'error_message': 'Mátricula ou senha incorreto.'})
-    else:  
-        return render(request, 'pages/home.html')
-
-    
-
-
+        # print(user)
+        # if user is not None:
+        # # Authenticates the user in the admin session
+        #     # login(request, user)
+        #     return render(request, 'menu.html')
+        # else:
+        #     return render(request, 'pages/home.html', {'error_message': 'Mátricula ou senha incorreto.'})
+    else:
+        return render(request, "pages/home.html")
 
 
 # Identificar se o usuário existe
 # Configuração do Menu
 def menu(request):
-        return render(request, 'menu.html')
+    return render(request, "panel.html")
+
 
 def rendimento(request):
-            return render(request, 'rendimento.html')
-       
+    return render(request, "rendimento.html")
+
 
 def holerite(request):
-        return render(request, 'holerite.html')
+    return render(request, "holerite.html")
 
-                
+
 # Configuração do Menu
+
 
 # voltar
 def voltar(request):
     request.session.clear()
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
+
 # voltar
-
-
-
